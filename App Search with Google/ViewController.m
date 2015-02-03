de3fd13b471ef451e36985d4f73473948b2de518
Joel Fischer <joel@livioradio.com>
2015-02-03 11:46:13 -0500

    Fix cancel bug
    Add some delegate search bar methods

--- App Search with Google/ViewController.m
+++ App Search with Google/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UISearchControllerDelegate, UISearchResultsUpdating>
+@interface ViewController () <UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate>
 
 @property (nonatomic, strong) UISearchController *searchController;
 
@@ -19,14 +19,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    self.searchController = [[UISearchController alloc] initWithSearchResultsController:self];
+    
+    // Set up the search controller
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
     self.searchController.delegate = self;
     self.searchController.hidesNavigationBarDuringPresentation = NO;
     self.searchController.searchResultsUpdater = self;
     self.navigationItem.titleView = self.searchController.searchBar;
     
-    
+    // Set up the search bar
+    UISearchBar *searchBar = self.searchController.searchBar;
+    searchBar.delegate = self;
+    searchBar.placeholder = NSLocalizedString(@"Search for an App", nil);
+    [searchBar setKeyboardAppearance:UIKeyboardAppearanceDark];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,10 +39,62 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UISearchControllerDelegate
+
+//- (void)willPresentSearchController:(UISearchController *)searchController {
+//    
+//}
+//
+//- (void)didPresentSearchController:(UISearchController *)searchController {
+//    
+//}
+//
+//- (void)willDismissSearchController:(UISearchController *)searchController {
+//    
+//}
+//
+//- (void)didDismissSearchController:(UISearchController *)searchController {
+//    
+//}
+
+// Called after the search controller's search bar has agreed to begin editing or when 'active' is set to YES. If you choose not to present the controller yourself or do not implement this method, a default presentation is performed on your behalf.
+//- (void)presentSearchController:(UISearchController *)searchController {
+//    
+//}
+
+
 #pragma mark - UISearchResultsUpdating
 
 - (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
     NSLog(@"Update Search Results for Search Controller: %@", searchController);
+}
+
+
+// TODO: Separate objects to handle this stuff, similar to table view data sources?
+#pragma mark - UISearchBarDelegate
+
+// called when text starts editing
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    
+}
+
+// called when text ends editing
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+    
+}
+
+// called when text changes (including clear)
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    NSLog(@"Search Bar Text did Change: %@", searchText);
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    NSLog(@"Search Bar Search Button Clicked");
+}
+
+// called when cancel button pressed
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    NSLog(@"Search Bar Cancel Clicked");
 }
 
 @end
